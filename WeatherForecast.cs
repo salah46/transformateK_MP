5ca91fd23945cb65f309018eac93e3c4a331b1f3
Garroudji_salah <garroudjimohamedse@gmail.com>
2023-10-31 17:31:44 +0100

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace transformatek_MP;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
